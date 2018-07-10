abd35f32019ca2c111959d0bc21c589c9b72b2d7
JINSUNG\dnghwls7 <dnghwls7@ajou.ac.kr>
2018-07-10 10:59:47 +0900

    oauth facebook setup

--- conboard/app/models/user.rb
+++ conboard/app/models/user.rb
@@ -2,12 +2,13 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable,
+         :omniauthable, omniauth_providers: [:facebook]
 
   has_many :posts
   has_many :comments
   has_many :likes
-  #liked_post : 좋아요를 한 게시글, 
+  #liked_post : 좋아요를 한 게시글,
   # through : reference정보(외래키)가 like에 저장되어 있기 때문에 - 원하는 걸 찾아서 가져올 수 있도록
   has_many :liked_posts, through: :likes, source: :post
 
--- conboard/config/routes.rb
+++ conboard/config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    omniauth_callbacks: 'users/omniauth_callbacks'
+  }
   root 'posts#index'
   resources :posts
   post '/posts/:post_id/comments' => 'comments#create'
   # get '/posts/:post_id/likeA' => 'likes#like'
   put '/posts/:post_id/like' => 'likes#create'
   delete '/posts/:post_id/like' => 'likes#destroy'
+
+  post '/tinymce_assets' => 'tinymce_assets#create'
 end
